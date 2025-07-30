d9227b0c528ff4c9d807ae26647fdcf3270d246b
Zahoor Khan <zahoorkhan112257@gmail.com>
2025-07-30 14:31:05 +0500

    chang ing about and nav css

--- fornt-end/src/components/Image.container.js
+++ fornt-end/src/components/Image.container.js
@@ -28,7 +28,7 @@ const Image = () => {
             <div id='image-heading-description'>
                 <h2 id='image-heading'>Zahoor Khan</h2>
                 <p id='image-description'>Software Engineer ||
-Dynamic <span className='mern-span mern'>MERN</span> Stack Developer | Crafting Scalable, Responsive and single page Web Applications with <span className='mern-span'>JavaScript</span>, <span className='mern-span'>MongoDB</span>, <span className='mern-span'>Express</span>, <span className='mern-span'>React</span>, and <span className='mern-span'>Node.js</span></p>
+Dynamic <span className='mern-span mern'>MERN</span> Stack Developer || Crafting Scalable, Responsive and single page Web Applications with <span className='mern-span'>JavaScript</span>, <span className='mern-span'>MongoDB</span>, <span className='mern-span'>Express</span>, <span className='mern-span'>React</span>, and <span className='mern-span'>Node.js</span></p>
             </div>
             
 
