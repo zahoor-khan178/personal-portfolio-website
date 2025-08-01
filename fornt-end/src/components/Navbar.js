79551989e26bf0317345a7b950f3716408afb9e5
Zahoor Khan <zahoorkhan112257@gmail.com>
2025-08-01 09:39:39 +0500

    nav bar css update

--- fornt-end/src/components/Navbar.js
+++ fornt-end/src/components/Navbar.js
@@ -26,6 +26,7 @@ const Navbar = () => {
           <li className="links-lis"><a href="#about" onClick={toggleNavbar} className="links">About</a></li>
           <li className="links-lis"><a href="/" onClick={toggleNavbar} className="links">Projects</a></li>
           <li className="links-lis"><a href="/" onClick={toggleNavbar} className="links">Experience</a></li>
+          <li className="links-lis"><a href="/" onClick={toggleNavbar} className="links">Skills</a></li>
           <li className="links-lis"><a href="/" onClick={toggleNavbar} className="links">Services</a></li>
           <li className="links-lis"><a href="/" onClick={toggleNavbar} className="links">Contact</a></li>
         </ul>
