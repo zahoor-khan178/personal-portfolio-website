1efd9242d3b36cf669a8fa34629598d577e4bb25
Zahoor Khan <zahoorkhan112257@gmail.com>
2025-08-18 19:17:02 +0500

    initial working on skills section

--- fornt-end/src/components/Skills.js
+++ fornt-end/src/components/Skills.js
@@ -7,7 +7,35 @@ const Skils = () => {
         <div className="skills-section" id="skills">
             <div className="skills-container">
         <h1 className="skills-heading">Skills</h1>
-        <div className='front-end-section'>Front</div>
+        <div className='front-end-section'>
+            <h3>Front</h3>
+            <div className='html-container front-end-subsections'>
+                <h4>HTML</h4>
+                <p> This CRUD application allows users to manage their registration details effectively. The project features two main pages: one for inserting and updating user information (name, email, and password) and another for viewing and deleting existing records in a tabular format.</p>
+            </div>
+            <div className='css-container front-end-subsections'>
+
+                <h4>CSS</h4>
+                <p> This CRUD application allows users to manage their registration details effectively. The project features two main pages: one for inserting and updating user information (name, email, and password) and another for viewing and deleting existing records in a tabular format.</p>
+            </div>
+            <div className='bootstrape-container front-end-subsections'>
+
+                <h4>BOOTSTRAPE</h4>
+                <p> This CRUD application allows users to manage their registration details effectively. The project features two main pages: one for inserting and updating user information (name, email, and password) and another for viewing and deleting existing records in a tabular format.</p>
+            </div>
+            <div className='javascript-container front-end-subsections'>
+
+                <h4>JAVASCRIPT</h4>
+                <p> This CRUD application allows users to manage their registration details effectively. The project features two main pages: one for inserting and updating user information (name, email, and password) and another for viewing and deleting existing records in a tabular format.</p>
+            </div>
+            <div className='react-container front-end-subsections'>
+
+                <h4>REACT.JS</h4>
+                <p> This CRUD application allows users to manage their registration details effectively. The project features two main pages: one for inserting and updating user information (name, email, and password) and another for viewing and deleting existing records in a tabular format.</p>
+            </div>
+
+
+        </div>
         
         </div>
         </div>
